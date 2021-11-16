645767e7be29a9cbb7da9a453a39e73445feb574
NagaRanjitha <87060453+Chinni7@users.noreply.github.com>
2021-11-16 10:53:18 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const getFirstNames = (list) => {
-  return list.map((eachPerson) => eachPerson.firstName);
+const peopleNames = require("../country/state/city/index");
+const getFirstNames = require("../utilities/utils/index");
+
+const getPeopleInCity = (peopleNames) => {
+  return getFirstNames(peopleNames);
 };
 
-module.exports = getFirstNames;
+module.exports = getPeopleInCity;
